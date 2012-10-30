d5711ad5c909f4d090d1ab4a91c46e9932204ae9
Grigoris <grchrysos@yahoo.gr>
2012-10-30 22:11:16 +0200

    This refers to the crontab of linux
    If you want to schedule some tasks that linux could execute by
    themselves then crontab is a solution. However, I didn't manage to find
    in the internet how  exactly to fix the code, so this is the code that
    worked in the end.
    There is also some example code that you can execute.

---
+++ crontab_linux/mybash.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+/bin/echo "This Process ID is [$$] new" >/home/user/Documents/mysave.txt
+
+
+/usr/bin/xdg-open https://github.com/grigorisg9gr
+
+/bin/sleep 10
+
+
+/bin/echo "$$ End" >/home/user/Documents/pi11.txt
+
+
+
+
+
+
+
+
